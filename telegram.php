a1135d729aef357fa17dddd88bab14c246e890e8
KOLIA HA <nikolay@gomobile.ru>
2020-10-25 22:58:43 +0300

    first commit

---
+++ telegram.php
@@ -0,0 +1,32 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+$token = "1279204731:AAHsD4YMv633i_5Zuo12M61Rqju1Ynv2V5I";
+$chat_id = "-469178035";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Сообщение:' => $msg
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+//$sendToTelegram2 = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: Thanks.html');
+} else {
+  echo "Error";
+}
+?>
